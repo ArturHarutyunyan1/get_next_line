1f31c8bf9765d0a4ba7bdc85014d4e7aa68a01f6
Artur Harutyunyan <arturhar@outlook.com>
2024-02-19 10:06:20 +0400

    helper functions

--- get_next_line.h
+++ get_next_line.h
@@ -12,5 +12,10 @@
 # include <string.h>
 
 char	*get_next_line(int fd);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
+void	*ft_memset(void *str, int c, size_t n);
+void	*ft_calloc(size_t items, size_t size);
+size_t	ft_strlen(const char *str);
 
 #endif
